89b7112615dda3b339f016555bbf3c124349d323
Rafael Lazarini <joao.lazarini@isobar.com.br>
2017-01-30 16:48:11 -0200

    Correcoes da api

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-function both(method, serviceName, data, urlComplement, fallBackJson){
+function both(method, url, data, fallback){
 
     return new Promise(function(resolve, reject){
-        /*************** Dependencies ***************/
-            var config = require('../config')
-        /********************************************/
 
-        data = data ? jsonToFormData(data) : '';
+        data = data ? $jsonToFormData(data) : '';
         if(method == 'POST') data = data.replace(/^\?/, '');
-        urlComplement = urlComplement ? ('/' + urlComplement) : '';
-        service = serviceName.match(/^(https?:)?\/\//) ? serviceName : !fallBackJson ? config.services[serviceName].url : config.services[serviceName]['fallBack'] + '/' + fallBackJson +  '.json';
 
-        if(!service) return fail('Service name unknown');
+        if(!url) return fail('Service name unknown');
 
         try {
             var xhr = new XMLHttpRequest();
-            service = (method == 'POST') ? service : service + urlComplement + data;
-            xhr.open(method, service);
+            url = (method == 'POST') ? url : url + data;
+            xhr.open(method, url);
             method == 'POST' && xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
             xhr.onreadystatechange = function(){
                 if(xhr.readyState != 4) return;
                 if(xhr.status == 200) return resolve(xhr.responseText);
-                return fail({ status : xhr.status, data : xhr.responseText });
+                if(!!fallback && xhr.status != 200) return resolve($tryFallback(method, (fallback || url), (!!fallback ? true : false), data, { 'status' : xhr.status, 'data' : xhr.responseText, 'url' : url }));
+                return fail({ 'status' : xhr.status, 'data' : xhr.responseText, 'url' : url });
             }
             xhr.send(data);
         } catch (err) {
             var xdr = new XDomainRequest();
-            xdr.open(method, service + urlComplement + data);
+            xdr.open(method, url + data);
             xdr.ontimeout = fail;
             xdr.onerror = fail;
             xdr.onprogress = progress;
@@ -55,7 +51,21 @@ function post(serviceName, data){
     return both('POST', serviceName, data);
 }
 
-function jsonToFormData(obj){
+function $tryFallback(method, url, fallback, data, err){
+    if (typeof err === 'object' ) err.message = (fallback) ? "Service error, trying the fallback" : "";
+    console.error('status: ', err.status, '\n', 'data: ', err.data, '\n', 'message: ', err.message, '\n', 'url: ', err.url)
+    if (fallback) {
+        return both(method, url, data);
+    }
+    //.then(function(data){
+        //     reject(data);
+        // }, function(data) {
+        //     reject(Error("It broke"));
+        // })
+}
+
+function $jsonToFormData(obj){
+    if (typeof obj !== 'object') return obj;
     var paramString = '?';
     for (var key in obj) {
         var value = obj[key];
@@ -79,3 +89,40 @@ try {
     // using like a library
     window.apijs = apijs;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+apijs.get('https://jsonplaceholder.typicode.com/posts/null', {'id':'1'}, 'https://jsonplaceholder.typicode.com/posts/nudes').then(function(resolve){
+    console.log("then :: ", resolve);
+}, function(reject){
+    console.log("reject :: ", reject);
+});
